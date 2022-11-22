5d52578dd91b0500141ee7fbae3a30141ea160f3
Semyonenko Ivan <vsem2004@gmail.com>
2022-11-22 12:56:37 +0300

    add sticky time panel

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import "./style.css";
 import Header from "../Header";
 import SignInPopup from "../SignInPopup";
@@ -8,13 +8,31 @@ const Main = () => {
     const [isVisible, setIsVisible] = useState(false);
     const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth") || false);
 
-    console.log(isAuth);
+    // const onEscClick = useCallback((evt) => {
+    //     if (evt.key === "Escape") {
+    //         setIsVisible(true);
+    //     }
+    // }, []);
+
+    // useEffect(() => {
+    //     document.addEventListener("keypress", onEscClick, false);
+
+    //     return () => {
+    //         document.removeEventListener("keypress", onEscClick, false);
+    //     };
+    // }, [onEscClick]);
+
+    document.addEventListener('keypress', (evt) => {
+        console.log(evt.key === "Escape");
+    })
+
+    console.log('user auth status:' + isAuth);
 
     return (
         <>
             <Header setIsVisible={setIsVisible} isAuth={isAuth} setIsAuth={setIsAuth} />
             <SignInPopup isVisible={isVisible} setIsVisible={setIsVisible} setIsAuth={setIsAuth} />
-            <Calendar/>
+            <Calendar />
             {isVisible && <div onClick={() => setIsVisible(false)} className="overlay"></div>}
         </>
     );
