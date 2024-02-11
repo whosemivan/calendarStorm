83c1747d7da32a592c67d88f243fa7bad90b7342
KLIKVUS <74531933+KLIKVUS@users.noreply.github.com>
2024-02-11 19:25:53 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import browserHistory from './browser-history';
 
 class Api {
     constructor() {
-        this.url = "https://calendar-storm.ru//api/";
+        this.url = "https://calendar-storm.ru/api/";
     }
 
     logIn(body) {
